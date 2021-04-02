8e2eda18ce80b8ede7b10670160ed44cf1290015
Go Sagawa <drunkturtle@gmail.com>
2021-04-02 11:27:04 +0900

    wip abc197 e

--- examples/stdin.go
+++ examples/stdin.go
@@ -9,6 +9,8 @@ import (
 var wtr = bufio.NewWriter(os.Stdout)
 
 func main() {
+
+	// defaut
 	var a, b, c int
 	var s string
 	fmt.Scanf("%d", &a)
@@ -16,6 +18,15 @@ func main() {
 	fmt.Scanf("%s", &s)
 	fmt.Printf("%d %s\n", a+b+c, s)
 
+	// get with loop
+	var n int
+	fmt.Scanf("%d", &n)
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &nums[i])
+	}
+
+	// output
 	fmt.Fprintln(wtr, "Hello")
 	fmt.Fprintln(wtr, "World")
 	_ = wtr.Flush()
