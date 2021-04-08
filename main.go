007420342877b27e90a403b6cd26fac3703837ef
Go Sagawa <drunkturtle@gmail.com>
2021-04-08 09:15:46 +0900

    wip

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
+	"math"
+	"math/bits"
 	"os"
+	"runtime/pprof"
 	"strconv"
 	"strings"
 )
@@ -13,6 +16,16 @@ var sc = bufio.NewScanner(os.Stdin)
 var wtr = bufio.NewWriter(os.Stdout)
 
 func main() {
+
+	f, err := os.Create("cpu.pprof")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := pprof.StartCPUProfile(f); err != nil {
+		panic(err)
+	}
+	defer pprof.StopCPUProfile()
 
 	if len(os.Args) > 1 && os.Args[1] == "i" {
 		b, e := ioutil.ReadFile("./input")
@@ -23,10 +36,54 @@ func main() {
 	}
 	sc.Buffer(make([]byte, 10000), 2000000)
 
-	slen, s := stouis(ns())
-	tlen, t := stouis(ns())
+	s := nb()
+	t := nb()
+	slen := len(s)
+	tlen := len(t)
 
-	fmt.Println(maxdistance(s, t, slen, tlen))
+	tuintlistlen := (tlen-1)/64 + 1
+	tuintlist := make([]uint64, tuintlistlen, tuintlistlen)
+	tuintindex := -1
+	for i, c := range t {
+		if i%64 == 0 {
+			tuintindex++
+		}
+
+		d := c - '0'
+		tuintlist[tuintindex] *= uint64(2)
+		n1 := tuintlist[tuintindex] + uint64(d)
+		tuintlist[tuintindex] = n1
+	}
+	alllen := slen - tlen + 1
+	suintalllist := make([][]uint64, alllen, alllen)
+	suintalllistindex := make([]int, alllen, alllen)
+	suintalllistcounter := make([]int, alllen, alllen)
+	for ai := 0; ai < alllen; ai++ {
+		suintalllist[ai] = make([]uint64, tuintlistlen, tuintlistlen)
+		suintalllistindex[ai] = -1
+		suintalllistcounter[ai] = 0
+	}
+	bktree := &BKTree{}
+	for i, c := range s {
+		for ai := 0; ai < alllen; ai++ {
+			if 0 <= i-ai && i-ai < tlen {
+				if suintalllistcounter[ai]%64 == 0 {
+					suintalllistindex[ai]++
+				}
+				suintindex := suintalllistindex[ai]
+				d := c - '0'
+				suintalllist[ai][suintindex] *= uint64(2)
+				n1 := suintalllist[ai][suintindex] + uint64(d)
+				suintalllist[ai][suintindex] = n1
+				suintalllistcounter[ai]++
+				if i-ai == tlen-1 {
+					bktree.Add(suintalllist[ai])
+				}
+			}
+		}
+	}
+
+	fmt.Println(bktree.Search(tuintlist))
 	_ = wtr.Flush()
 }
 
@@ -62,100 +119,6 @@ func nb() []byte {
 	return sc.Bytes()
 }
 
-func stouis(s string) (int, []uint64) {
-	ls := len(s)
-	c := (ls-1)/64 + 1
-	uis := make([]uint64, c, c)
-	for i := 0; i < c; i++ {
-		f := ls - (i+1)*64
-		t := ls - i*64
-		if f < 0 {
-			f = 0
-		}
-		if t < 0 {
-			t = ls
-		}
-		subs := s[f:t]
-		u, e := strconv.ParseUint(subs, 2, 64)
-		if e != nil {
-			panic(e)
-		}
-		uis[i] = u
-	}
-	return ls, uis
-}
-
-func maxdistance(sl, tl []uint64, ss, ts int) int {
-
-	d := ts
-	var k uint64 = 1
-	var sActive uint64
-	allc := ss - ts + 1
-	var checkerC, slcounter int
-
-	checker := make([]uint64, ts, ts)
-	checkerd := make([]int, ts, ts)
-	checkeri := make([]int, ts, ts)
-
-	activeChecker := 1
-	for si := 0; si < ss; si++ {
-		if si%64 == 0 {
-			sActive = sl[slcounter]
-			slcounter++
-		}
-		if si < allc {
-			checkerd[checkerC] = 0
-			checkeri[checkerC] = 0
-		}
-		for c := 0; c < activeChecker; c++ {
-			if checkeri[c] == ts {
-				continue
-			}
-
-			if checkeri[c]%64 == 0 {
-				nextI := checkeri[c] / 64
-				checker[c] = tl[nextI]
-			}
-
-			if checker[c]&k != sActive&k {
-				checkerd[c]++
-			}
-
-			checker[c] >>= 1
-			checkeri[c]++
-		}
-		/*
-		           fmt.Println("--------------")
-		   		fmt.Println(checker)
-		   		fmt.Println(checkerd)
-		   		fmt.Println(checkeri)
-		*/
-		if si >= ts-1 {
-			finishI := (si - ts + 1) % ts
-			/*
-				fmt.Println("-------")
-				fmt.Println(finishI, checkerd[finishI])
-			*/
-			if checkerd[finishI] < d {
-				d = checkerd[finishI]
-			}
-		}
-
-		if si < ts-1 && si < allc-1 {
-			activeChecker++
-		}
-
-		sActive >>= 1
-
-		if si%ts == ts-1 {
-			checkerC = 0
-		} else {
-			checkerC++
-		}
-
-	}
-	return d
-}
 func distance(a, b uint64) int {
 
 	d := 0
@@ -167,4 +130,83 @@ func distance(a, b uint64) int {
 		k <<= 1
 	}
 	return d
+}
+
+type BKTree struct {
+	root *node
+}
+
+type node struct {
+	entry    hashValue
+	children []struct {
+		distance int
+		node     *node
+	}
+}
+
+func (n *node) addChild(e hashValue) {
+	newnode := &node{entry: e}
+loop:
+	d := n.entry.Distance(e)
+	for _, c := range n.children {
+		if c.distance == d {
+			n = c.node
+			goto loop
+		}
+	}
+	n.children = append(n.children, struct {
+		distance int
+		node     *node
+	}{d, newnode})
+}
+
+func (bk *BKTree) Add(entry hashValue) {
+	if bk.root == nil {
+		bk.root = &node{
+			entry: entry,
+		}
+		return
+	}
+	bk.root.addChild(entry)
+}
+
+type hashValue []uint64
+
+func (h hashValue) Distance(e hashValue) int {
+	a := []uint64(h)
+	b := []uint64(e)
+
+	d := 0
+	for i, v := range a {
+		d += bits.OnesCount64(v ^ b[i])
+	}
+	return d
+}
+
+func (bk *BKTree) Search(needle hashValue) int {
+
+	result := 1000000
+	for i := 0; i <= 100000; i++ {
+		tolerance := int(math.Pow(2, float64(i)))
+		candidates := []*node{bk.root}
+		for len(candidates) != 0 {
+			c := candidates[len(candidates)-1]
+			candidates = candidates[:len(candidates)-1]
+			d := c.entry.Distance(needle)
+			if d <= tolerance && d < result {
+				result = d
+			}
+
+			low, high := d-tolerance, d+tolerance
+			for _, c := range c.children {
+				if low <= c.distance && c.distance <= high {
+					candidates = append(candidates, c.node)
+				}
+			}
+		}
+		if result != 1000000 {
+			break
+		}
+	}
+	return result
 }
