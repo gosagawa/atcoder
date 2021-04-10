bfb35747b2d99796d8147364c558d6368176b8cd
Go Sagawa <drunkturtle@gmail.com>
2021-04-10 19:17:38 +0900

    abc081 a

---
+++ _abc081/a/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+
+	// defaut
+	a := ns()
+	var o int
+	for _, v := range a {
+		if v == '1' {
+			o++
+		}
+	}
+
+	// output
+	fmt.Fprintln(wtr, o)
+	_ = wtr.Flush()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
