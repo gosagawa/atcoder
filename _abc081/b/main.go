689918bead4322581960701c7177f96ccbb9a3d1
Go Sagawa <drunkturtle@gmail.com>
2021-04-10 19:45:19 +0900

    adc081 b

---
+++ _abc081/b/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+
+	// get with loop
+	n := ni()
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		nums[i] = ni()
+	}
+	o := 0
+
+loop:
+	for i := 1; i <= 64; i++ {
+		for j, v := range nums {
+			if v%2 != 0 {
+				break loop
+			}
+			nums[j] /= 2
+		}
+		o = i
+	}
+
+	// output
+	fmt.Fprintln(wtr, o)
+	_ = wtr.Flush()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
