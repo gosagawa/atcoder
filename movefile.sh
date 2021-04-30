c67445b326b5529514f8ef966d121b7fddefc4c4
Go Sagawa <drunkturtle@gmail.com>
2021-05-01 08:56:29 +0900

    make result dir

--- movefile.sh
+++ movefile.sh
@@ -4,8 +4,8 @@ echo -n DIR?
 read dir
 echo -n QUESTION?
 read q
-mkdir -p _$dir/$q
-mv -i main.go _$dir/$q
-git add _$dir/$q/main.go
+mkdir -p _result/_$dir/$q
+mv -i main.go _result/_$dir/$q
+git add _result/_$dir/$q/main.go
 git commit -m "$dir $q"
 
