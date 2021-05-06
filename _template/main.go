17f6c16eefb46fc433d09acad9194f88bc3378ab
Go Sagawa <drunkturtle@gmail.com>
2021-05-06 11:12:00 +0900

    update template

--- _template/main.go
+++ _template/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"math/bits"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,20 +19,25 @@ func main() {
 
 	defer flush()
 
-	a := ni()
-	s := ns()
 	o := 0
-	_ = a
-	_ = s
 
-	n := ni()
-	ns := make([]int, n)
-	for i := 0; i < n; i++ {
-		ns[i] = ni()
-	}
+	/*
+		a := ni()
+		s := ns()
+
+		n := ni()
+		ns := make([]int, n)
+		for i := 0; i < n; i++ {
+			ns[i] = ni()
+		}
+	*/
 
 	out(o)
 }
+
+// ==================================================
+// init
+// ==================================================
 
 func init() {
 	sc.Buffer([]byte{}, math.MaxInt64)
@@ -44,6 +51,10 @@ func init() {
 	}
 }
 
+// ==================================================
+// io
+// ==================================================
+
 func ni() int {
 	sc.Scan()
 	i, e := strconv.Atoi(sc.Text())
@@ -51,6 +62,26 @@ func ni() int {
 		panic(e)
 	}
 	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func nis(n int) sort.IntSlice {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return sort.IntSlice(a)
 }
 
 func nf() float64 {
@@ -79,4 +110,154 @@ func flush() {
 	if e != nil {
 		panic(e)
 	}
+}
+
+// ==================================================
+// num
+// ==================================================
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func ch(cond bool, ok, ng int) int {
+	if cond {
+		return ok
+	}
+	return ng
+}
+
+func mul(a, b int) (int, int) {
+	if a < 0 {
+		a, b = -a, -b
+	}
+	if a == 0 || b == 0 {
+		return 0, 0
+	} else if a > 0 && b > 0 && a > math.MaxInt64/b {
+		return 0, +1
+	} else if a < math.MinInt64/b {
+		return 0, -1
+	}
+	return a * b, 0
+}
+
+// ==================================================
+// binarysearch
+// ==================================================
+
+func binarysearch(ok, ng int, judge func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ==================================================
+// bit
+// ==================================================
+
+// nthbit はaのn番目のビットを返します。
+func nthbit(a int, n int) int { return int((a >> uint(n)) & 1) }
+
+// popcount はaのうち立っているビットを数えて返します。
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+func xor(a, b bool) bool { return a != b }
+
+// ==================================================
+// string
+// ==================================================
+
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// slice
+// ==================================================
+
+func reverse(arr *[]interface{}) {
+	for i, j := 0, len(*arr)-1; i < j; i, j = i+1, j-1 {
+		(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
+	}
+}
+
+func reverseInt(arr *[]int) {
+	for i, j := 0, len(*arr)-1; i < j; i, j = i+1, j-1 {
+		(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
+	}
+}
+
+func uniqueInt(arr []int) []int {
+	hist := map[int]bool{}
+	j := 0
+	for i := 0; i < len(arr); i++ {
+		if hist[arr[i]] {
+			continue
+		}
+
+		a := arr[i]
+		arr[j] = a
+		hist[a] = true
+		j++
+	}
+	return arr[:j]
+}
+
+// ==================================================
+// point
+// ==================================================
+
+// Point is int point
+type Point struct {
+	x int
+	y int
+}
+
+// Pointf is float point
+type Pointf struct {
+	x float64
+	y float64
 }
