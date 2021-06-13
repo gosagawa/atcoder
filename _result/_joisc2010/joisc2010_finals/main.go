60ab750e8b43e6a7605938c3bb05db810db00c23
Go Sagawa <drunkturtle@gmail.com>
2021-06-13 11:30:24 +0900

    joisc2010 joisc2010_finals

---
+++ _result/_joisc2010/joisc2010_finals/main.go
@@ -0,0 +1,724 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"math/bits"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	defer flush()
+
+	o := 0
+
+	v, e, k := ni3()
+	edges := make([][]edge, 1)
+	edges[0] = make([]edge, e)
+
+	for i := 0; i < e; i++ {
+		s, t, d := ni3()
+		s--
+		t--
+		edges[0][i] = edge{from: s, to: t, cost: d}
+	}
+
+	graph := newgraph(v, edges)
+
+	o = graph.kruskalEx(k)
+	out(o)
+}
+
+func (g *graph) kruskalEx(k int) int {
+
+	sort.Slice(g.edges[0], func(i, j int) bool { return g.edges[0][i].cost < g.edges[0][j].cost })
+
+	e := len(g.edges[0])
+
+	uf := newUnionFind(g.size)
+	r := 0
+	part := g.size
+	for i := 0; i < e; i++ {
+		if part == k {
+			break
+		}
+		edge := g.edges[0][i]
+		if uf.issame(edge.from, edge.to) {
+			continue
+		}
+		r += edge.cost
+		uf.unite(edge.from, edge.to)
+		part--
+	}
+
+	return r
+}
+
+// ==================================================
+// init
+// ==================================================
+
+const inf = math.MaxInt64
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+	if len(os.Args) > 1 && os.Args[1] == "i" {
+		b, e := ioutil.ReadFile("./input")
+		if e != nil {
+			panic(e)
+		}
+		sc = bufio.NewScanner(strings.NewReader(strings.Replace(string(b), " ", "\n", -1)))
+	}
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2() (int, int) {
+	return ni(), ni()
+}
+
+func ni3() (int, int, int) {
+	return ni(), ni(), ni()
+}
+
+func ni4() (int, int, int, int) {
+	return ni(), ni(), ni(), ni()
+}
+
+func nis(n int) sort.IntSlice {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni()
+	}
+	return sort.IntSlice(a)
+}
+
+func nf() float64 {
+	sc.Scan()
+	f, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return f
+}
+
+func ns() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func out(v ...interface{}) {
+	_, e := fmt.Fprintln(wtr, v...)
+	if e != nil {
+		panic(e)
+	}
+}
+
+func flush() {
+	e := wtr.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+// ==================================================
+// num
+// ==================================================
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func pow2(a int) int {
+	return int(math.Pow(2, float64(a)))
+}
+
+func ch(cond bool, ok, ng int) int {
+	if cond {
+		return ok
+	}
+	return ng
+}
+
+func mul(a, b int) (int, int) {
+	if a < 0 {
+		a, b = -a, -b
+	}
+	if a == 0 || b == 0 {
+		return 0, 0
+	} else if a > 0 && b > 0 && a > math.MaxInt64/b {
+		return 0, +1
+	} else if a < math.MinInt64/b {
+		return 0, -1
+	}
+	return a * b, 0
+}
+
+func permutation(n int, k int) int {
+	if k > n || k <= 0 {
+		panic(fmt.Sprintf("invalid param n:%v k:%v", n, k))
+	}
+	v := 1
+	for i := 0; i < k; i++ {
+		v *= (n - i)
+	}
+	return v
+}
+
+/*
+	for {
+
+		// Do something
+
+		if !nextPermutation(sort.IntSlice(x)) {
+			break
+		}
+	}
+*/
+func nextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func combination(n int, k int) int {
+	if n-k < k {
+		k = n - k
+	}
+	v := 1
+	for i := 0; i < k; i++ {
+		v *= (n - i)
+		v /= (i + 1)
+	}
+	return v
+}
+
+func factorial(n int) int {
+	return permutation(n, n-1)
+}
+
+// ==================================================
+// binarysearch
+// ==================================================
+/*
+	f := func(c int) bool {
+		return false
+	}
+*/
+func bs(ok, ng int, f func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if f(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ==================================================
+// bit
+// ==================================================
+
+func hasbit(a int, n int) bool {
+	return (a>>uint(n))&1 == 1
+}
+
+func nthbit(a int, n int) int {
+	return int((a >> uint(n)) & 1)
+}
+
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+func xor(a, b bool) bool { return a != b }
+
+// ==================================================
+// string
+// ==================================================
+
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// slice
+// ==================================================
+
+func sorti(sl []int) {
+	sort.Sort(sort.IntSlice(sl))
+}
+
+func sortir(sl []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(sl)))
+}
+
+func reverse(sl []interface{}) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
+
+func reversei(sl []int) {
+	for i, j := 0, len(sl)-1; i < j; i, j = i+1, j-1 {
+		sl[i], sl[j] = sl[j], sl[i]
+	}
+}
+
+func uniquei(sl []int) []int {
+	hist := map[int]bool{}
+	j := 0
+	for i := 0; i < len(sl); i++ {
+		if hist[sl[i]] {
+			continue
+		}
+
+		a := sl[i]
+		sl[j] = a
+		hist[a] = true
+		j++
+	}
+	return sl[:j]
+}
+
+func delIdx(idx int, L []int) []int {
+	r := []int{}
+	r = append(r, L[:idx]...)
+	r = append(r, L[idx+1:]...)
+	return r
+}
+
+// ==================================================
+// point
+// ==================================================
+
+type point struct {
+	x int
+	y int
+}
+
+type pointf struct {
+	x float64
+	y float64
+}
+
+func (p point) isValid(x, y int) bool {
+	return 0 <= p.x && p.x < x && 0 <= p.y && p.y < y
+}
+
+func pointAdd(a, b point) point {
+	return point{a.x + b.x, a.y + b.y}
+}
+
+func pointDist(a, b point) float64 {
+	return math.Sqrt(float64((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y)))
+}
+
+func pointfDist(a, b pointf) float64 {
+	return math.Sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y))
+}
+
+type pointQueue struct {
+	pt []point
+}
+
+func (q *pointQueue) push(pt point) {
+	q.pt = append(q.pt, pt)
+}
+
+func (q *pointQueue) pop() (pt point) {
+	pt = q.pt[0]
+	q.pt = q.pt[1:]
+	return
+}
+
+func (q *pointQueue) len() int {
+	return len(q.pt)
+}
+
+// ==================================================
+// heap
+// ==================================================
+
+/*
+  h := &int2dHeap{&int2d{dist[r], r}}
+  heap.Init(h)
+  v := heap.Pop(h).(*int2d)
+  heap.Push(h, &int2d{x, y})
+*/
+
+type int2d [2]int
+
+type int2dHeap []*int2d
+
+func (h int2dHeap) Len() int           { return len(h) }
+func (h int2dHeap) Less(i, j int) bool { return h[i][0] < h[j][0] }
+func (h int2dHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *int2dHeap) Push(x interface{}) {
+	*h = append(*h, x.(*int2d))
+}
+
+func (h *int2dHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old[n-1] = nil
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *int2dHeap) IsEmpty() bool {
+	return h.Len() == 0
+}
+
+type pq struct {
+	arr   []interface{}
+	comps []compFunc
+}
+
+/*
+	graph.comps = []compFunc{
+		func(p, q interface{}) int {
+			if p.(state).score < q.(state).score {
+				return -1
+			} else if p.(state).score == q.(state).score {
+				return 0
+			}
+			return 1
+		},
+	}
+*/
+type compFunc func(p, q interface{}) int
+
+func newpq(comps []compFunc) *pq {
+	return &pq{
+		comps: comps,
+	}
+}
+
+func (pq pq) Len() int {
+	return len(pq.arr)
+}
+
+func (pq pq) Swap(i, j int) {
+	pq.arr[i], pq.arr[j] = pq.arr[j], pq.arr[i]
+}
+
+func (pq pq) Less(i, j int) bool {
+	for _, comp := range pq.comps {
+		result := comp(pq.arr[i], pq.arr[j])
+		switch result {
+		case -1:
+			return true
+		case 1:
+			return false
+		case 0:
+			continue
+		}
+	}
+	return true
+}
+
+func (pq *pq) Push(x interface{}) {
+	pq.arr = append(pq.arr, x)
+}
+
+func (pq *pq) Pop() interface{} {
+	n := pq.Len()
+	item := pq.arr[n-1]
+	pq.arr = pq.arr[:n-1]
+	return item
+}
+
+func (pq *pq) Top() interface{} {
+	n := pq.Len()
+	return pq.arr[n-1]
+}
+
+func (pq *pq) IsEmpty() bool {
+	return pq.Len() == 0
+}
+
+// ==================================================
+// union find
+// ==================================================
+
+type unionFind struct {
+	par []int
+}
+
+func newUnionFind(n int) *unionFind {
+	u := &unionFind{
+		par: make([]int, n),
+	}
+	for i := range u.par {
+		u.par[i] = -1
+	}
+	return u
+}
+
+func (u *unionFind) root(x int) int {
+	if u.par[x] < 0 {
+		return x
+	}
+	u.par[x] = u.root(u.par[x])
+	return u.par[x]
+}
+
+func (u *unionFind) unite(x, y int) {
+	x = u.root(x)
+	y = u.root(y)
+	if x == y {
+		return
+	}
+	if u.size(x) < u.size(y) {
+		x, y = y, x
+	}
+	u.par[y] += u.par[x]
+	u.par[x] = y
+}
+
+func (u *unionFind) issame(x, y int) bool {
+	if u.root(x) == u.root(y) {
+		return true
+	}
+	return false
+}
+
+func (u *unionFind) size(x int) int {
+	return -u.par[u.root(x)]
+}
+
+// ==================================================
+// graph
+// ==================================================
+
+type edge struct {
+	from int
+	to   int
+	cost int
+}
+
+type state struct {
+	score int
+	node  int
+}
+
+type graph struct {
+	size         int
+	edges        [][]edge
+	starts       []state
+	comps        []compFunc
+	defaultScore int
+}
+
+func newgraph(size int, edges [][]edge) *graph {
+	return &graph{
+		size:  size,
+		edges: edges,
+	}
+}
+
+func (g *graph) setStart(s state) {
+	g.starts = append(g.starts, s)
+}
+
+/*
+	v, e, r := ni3()
+	edges := make([][]edge, v)
+
+	for i := 0; i < e; i++ {
+		s, t, d := ni3()
+		edges[s] = append(edges[s], edge{to: t, cost: d})
+	}
+
+	graph := newgraph(v, edges)
+	graph.setStart(state{node: r})
+	graph.defaultScore = inf
+	graph.comps = []compFunc{
+		func(p, q interface{}) int {
+			if p.(state).score < q.(state).score {
+				return -1
+			} else if p.(state).score == q.(state).score {
+				return 0
+			}
+			return 1
+		},
+	}
+	dist := graph.dijkstra()
+*/
+
+func (g *graph) dijkstra() []int {
+	score := make([]int, g.size)
+	for i := 0; i < g.size; i++ {
+		score[i] = g.defaultScore
+	}
+	que := newpq(g.comps)
+	for _, start := range g.starts {
+		score[start.node] = start.score
+		heap.Push(que, start)
+	}
+
+	for !que.IsEmpty() {
+		st := heap.Pop(que).(state)
+		if st.score > score[st.node] {
+			continue
+		}
+		for _, edge := range g.edges[st.node] {
+			newScore := st.score + edge.cost
+			if score[edge.to] > newScore {
+				score[edge.to] = newScore
+				heap.Push(que, state{score: newScore, node: edge.to})
+			}
+		}
+	}
+	return score
+}
+
+func (g *graph) floydWarshall() ([][]int, bool) {
+
+	score := make([][]int, g.size)
+	for i := 0; i < g.size; i++ {
+		score[i] = make([]int, g.size)
+		for j := 0; j < g.size; j++ {
+			if i == j {
+				score[i][j] = 0
+			} else {
+				score[i][j] = g.defaultScore
+			}
+		}
+		for _, edge := range g.edges[i] {
+			score[i][edge.to] = edge.cost
+		}
+	}
+	for k := 0; k < g.size; k++ {
+		for i := 0; i < g.size; i++ {
+			for j := 0; j < g.size; j++ {
+				if score[i][k] == g.defaultScore || score[k][j] == g.defaultScore {
+					continue
+				}
+				score[i][j] = min(score[i][j], score[i][k]+score[k][j])
+			}
+		}
+	}
+
+	for k := 0; k < g.size; k++ {
+		if score[k][k] < 0 {
+			return nil, true
+		}
+	}
+
+	return score, false
+}
+
+/*
+	v, e := ni2()
+	edges := make([][]edge, 1)
+	edges[0] = make([]edge, e)
+
+	for i := 0; i < e; i++ {
+		s, t, d := ni3()
+		edges[0][i] = edge{from: s, to: t, cost: d}
+	}
+
+	graph := newgraph(v, edges)
+
+	o = graph.kruskal()
+*/
+func (g *graph) kruskal() int {
+
+	sort.Slice(g.edges[0], func(i, j int) bool { return g.edges[0][i].cost < g.edges[0][j].cost })
+
+	e := len(g.edges[0])
+
+	uf := newUnionFind(g.size)
+	r := 0
+	for i := 0; i < e; i++ {
+		edge := g.edges[0][i]
+		if uf.issame(edge.from, edge.to) {
+			continue
+		}
+		r += edge.cost
+		uf.unite(edge.from, edge.to)
+	}
+
+	return r
+}
