630a6c53b0517a7455aaa971040603baa064af42
Go Sagawa <drunkturtle@gmail.com>
2022-02-12 16:08:19 +0900

    APG4b cs

---
+++ _result/_APG4b/cs/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 一年の秒数
+    int seconds = 365 * 24 * 60 * 60;
+
+    // 以下のコメント/* */を消して追記する
+    cout << seconds << endl;
+    cout << seconds * 2 << endl;
+    cout << seconds * 5 << endl;
+    cout << seconds * 10 << endl;
+}
