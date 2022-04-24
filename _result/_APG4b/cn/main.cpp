6d2602d8b26691ad0bc8e1870ee5beb4ee514b58
Go Sagawa <drunkturtle@gmail.com>
2022-04-24 17:46:57 +0900

    apg4b cn

---
+++ _result/_APG4b/cn/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define out(X) cout << (X) << endl;
+#ifdef __LOCAL
+#define DBG(X) cout << #X << " = " << (X) << endl;
+#else
+#define DBG(X)
+#endif
+
+using namespace std;
+
+int main() {
+#ifdef __LOCAL
+    freopen("input", "r", stdin);
+#endif
+
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    out(a + b)
+}
